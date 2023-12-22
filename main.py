71f29350392155710a4fa088913ba9d4adb641d1
grondinm <myself@marcg.pizza>
2023-12-21 21:37:48 -0400

    Add Official Letter count solution as comment for reference

--- main.py
+++ main.py
@@ -32,3 +32,15 @@ def get_book_text(path):
 
 
 main()
+
+
+#"Official" Solution for easy reference
+#def get_chars_dict(text):
+#    chars = {}
+#    for c in text:
+#        lowered = c.lower()
+#        if lowered in chars:
+#            chars[lowered] += 1
+#        else:
+#            chars[lowered] = 1
+#    return chars
