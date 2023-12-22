ad7e8008b30e5bfa04409a5837bdb14992636985
grondinm <myself@marcg.pizza>
2023-12-21 20:28:08 -0400

    Count letters. Preliminary report

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ def main():
     text = get_book_text(book_path)
     print(text)
     count = word_count(text)
-    print(f"The number of words in the text is {count}")
+    letterc = letter_count(text)
+    report(book_path,count)
+    print(letterc)
 
 def word_count(text):
     words = text.split()
     return len(words)
+
+def letter_count(text):
+    text_lower = text.lower()
+    letter_dict = {}
+    for letter in ''.join("abcdefghijklmnopqrstuvwxyz"):
+        count = text_lower.count(letter)
+        if count != 0:
+            letter_dict[letter] = count
+    return letter_dict
+
+def report(book_path,count):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"The number of words in the text is {count}\n\n")
+
 
 
 def get_book_text(path):
